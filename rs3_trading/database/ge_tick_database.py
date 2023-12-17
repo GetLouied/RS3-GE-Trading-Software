b20756108ede0ba33f22951189e2ce09f424610f
Louis DeRienzo <derienzo_louis@yahoo.com>
2023-12-17 18:54:42 +0000

    query and database fixes

--- rs3_trading/database/ge_tick_database.py
+++ rs3_trading/database/ge_tick_database.py
@@ -3,18 +3,16 @@ from duckdb import DuckDBPyConnection
 
 
 def create_price_table(con: DuckDBPyConnection):
-    con.execute('create table if not exists rs3_price_data(time datetime, item varchar, item_name varchar, price long, primary key(item, time))')
+    con.execute('create table if not exists rs3_price_data(datetime_utc datetime, item varchar, item_name varchar, price long, primary key(item, datetime_utc))')
 
 
 def create_volume_table(con: DuckDBPyConnection):
-    con.execute('create table if not exists rs3_volume_data(time datetime, item varchar, item_name varchar, volume long, primary key(item, time))')
+    con.execute('create table if not exists rs3_volume_data(datetime_utc datetime, item varchar, item_name varchar, volume long, primary key(item, datetime_utc))')
 
 
 def insert_into_table(con: DuckDBPyConnection, table_name: str, column_names: list, dataframe: pd.DataFrame):
-    con.execute("""insert into {table_name}({column_names})
+    columns_str = ", ".join(column_names)
+    con.execute(f"""insert into {table_name}({columns_str})
             select
-                time,
-                item,
-                item_name,
-                price
-            from {dataframe}""")
+                {columns_str}
+            from dataframe""")
--- rs3_trading/utils/database_util.py
+++ rs3_trading/utils/database_util.py
@@ -1,11 +1,14 @@
 BASE_GE_DATABASE_NAME = r'rs3_{trading_data_type}_data'
-BASE_GE_COLUMN_NAMES = ['time', 'item', 'item_name', r'{trading_data_type}']
 
 
-class RS3TableBuilder:
-    def __init__(self, base_database_name: str = BASE_GE_DATABASE_NAME, base_column_names: list = BASE_GE_COLUMN_NAMES):
-        self.base_column_names = base_column_names
+class RS3TABLEDATATYPE:
+    Prices = 'price'
+    Volumes = 'volume'
+
+
+class RS3TableNameBuilder:
+    def __init__(self, base_database_name: str = BASE_GE_DATABASE_NAME):
         self.base_database_name = base_database_name
 
     def __call__(self, RS_data_type: str):
-        return self.base_column_names.format(trading_data_type=RS_data_type), self.base_database_name.format(trading_data_type=RS_data_type)
+        return self.base_database_name.format(trading_data_type=RS_data_type)
